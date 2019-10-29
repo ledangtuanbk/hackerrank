9e175d2be4c154abb2bc8cc7771b7dc55670181d
Le Dang Tuan <tledang@pentalog.com>
2019-10-29 15:47:21 +0700

    Java Regex

---
+++ src/com/ldt/java/JavaRegex.java
@@ -0,0 +1,25 @@
+package com.ldt.java;
+
+/**
+ * @author tledang
+ */
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+class JavaRegex{
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+class MyRegex{
+    String pattern = "^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+}
+
