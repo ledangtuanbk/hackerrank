0c87c7ea5fc0014fc870cae095b4985a9d474ef0
Le Dang Tuan <tledang@pentalog.com>
2019-11-01 13:49:13 +0700

    Java BigInteger

---
+++ src/com/ldt/java/JavaBigInteger.java
@@ -0,0 +1,23 @@
+package com.ldt.java;
+
+/**
+ * @author tledang
+ */
+//public class JavaBigInteger {
+//}
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class JavaBigInteger {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+        System.out.println(a.add(b));
+        System.out.println(a.multiply(b));
+    }
+}
+
+
